1c41c3658ff5a99d08b3801736f1b7b789016da2
Aominezone <taillepierret@hotmail.fr>
2024-12-29 09:15:27 +0100

    on se remets au même niveau que la branch distante avant de pull depuis develop dans le update.sh

--- 02-Software/23-008_API_ZT/update.sh
+++ 02-Software/23-008_API_ZT/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /Projets/23-008_API_ZT
-git stash save "message_de_votre_stash"
+git reset --hard
 git pull origin develop  # Remplace "develop" par la branche que tu utilises
 chmod +x update.sh
 
